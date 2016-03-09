05ce73631d2a73f503473db8004d93ae7e04a91d
shqcandy <jian_li_0705@hotmail.com>
2016-03-09 18:38:50 +0800

    408

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Review_408.cpp
+// Author      : shenq
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << 1.0 / 2 << endl;
+	int i;
+	cin >> i;
+	return 0;
+}
